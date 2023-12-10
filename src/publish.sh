603855037ecaa3b1876f33716aeab617f70ab2ce
Uladzislau Krejzo <vlad.czech2018@gmail.com>
2023-12-10 14:38:01 +0300

    Tryyyyy fix ghcr.io

--- src/publish.sh
+++ src/publish.sh
@@ -6,7 +6,7 @@ GITHUB_REPOSITORY=$(echo "$GITHUB_REPOSITORY" | awk '{print tolower($0)}')
 echo "VERSION=$VERSION"
 echo "OVERRIDE=$OVERRIDE"
 
-docker login ghcr.io -u "${GITHUB_ACTOR}" -p "${REPO_TOKEN}"
+docker login ghcr.io -u "${GITHUB_REF}" -p "${REPO_TOKEN}"
 
 VERSION=$VERSION docker-compose -f docker/docker-compose.yml -f "$OVERRIDE" up --no-start --remove-orphans
 IMAGES=$(docker inspect --format='{{.Image}}' "$(docker ps -aq)")
@@ -16,7 +16,7 @@ for IMAGE in $IMAGES; do
     echo "IMAGE: $IMAGE"
 
     NAME="jellyfin"
-    TAG="ghcr.io/${GITHUB_ACTOR}/$NAME:$VERSION"
+    TAG="ghcr.io/${GITHUB_REPOSITORY}/$NAME:$VERSION"
 
     docker tag "$IMAGE" "$TAG"
     docker push "$TAG"
